978e9701cbbe0c569dc216b2813548d81a45281d
Sergey Shchukin <s.j.schukin@yandex.ru>
2025-01-08 23:51:47 +0300

    added glightbox library

---
+++ web/themes/custom/kind_tan/js/details.js
@@ -0,0 +1,11 @@
+(function() {
+  "use strict";
+
+  /**
+   * Initiate glightbox
+   */
+  const glightbox = GLightbox({
+    selector: '.glightbox'
+  });
+
+})();
--- web/themes/custom/kind_tan/js/main.js
+++ web/themes/custom/kind_tan/js/main.js
@@ -84,4 +84,22 @@
   }
   window.addEventListener('load', aosInit);
 
+  /**
+   * Correct scrolling position upon page load for URLs containing hash links.
+   */
+  window.addEventListener('load', function(e) {
+    if (window.location.hash) {
+      if (document.querySelector(window.location.hash)) {
+        setTimeout(() => {
+          let section = document.querySelector(window.location.hash);
+          let scrollMarginTop = getComputedStyle(section).scrollMarginTop;
+          window.scrollTo({
+            top: section.offsetTop - parseInt(scrollMarginTop),
+            behavior: 'smooth'
+          });
+        }, 100);
+      }
+    }
+  });
+
 })();
