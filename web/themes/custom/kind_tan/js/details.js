3349e69a28d61125079e45fed3d1a5fa3d116f4e
Sergey Shchukin <s.j.schukin@yandex.ru>
2025-01-11 23:32:12 +0300

    added go-back button. Added camp shift displaying

--- web/themes/custom/kind_tan/js/details.js
+++ web/themes/custom/kind_tan/js/details.js
@@ -8,4 +8,13 @@
     selector: '.glightbox'
   });
 
+  /**
+   * Camp Shift Toggle
+   */
+  document.querySelectorAll('.camp-shift-item h3, .camp-shift-item .camp-shift-toggle').forEach(item => {
+    item.addEventListener('click', () => {
+      item.parentNode.classList.toggle('camp-shift-active');
+    });
+  });
+
 })();
