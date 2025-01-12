e08fc102bc8d2aac1cccf251a918103dde2964fe
Sergey Shchukin <s.j.schukin@yandex.ru>
2025-01-12 20:54:18 +0300

    added modal request form supporting

--- web/themes/custom/kind_tan/js/details.js
+++ web/themes/custom/kind_tan/js/details.js
@@ -11,10 +11,30 @@
   /**
    * Camp Shift Toggle
    */
-  document.querySelectorAll('.camp-shift-item h3, .camp-shift-item .camp-shift-toggle').forEach(item => {
+  document.querySelectorAll('.camp-shift-item h3, .camp-shift-item .camp-shift-toggle').forEach((item) => {
     item.addEventListener('click', () => {
       item.parentNode.classList.toggle('camp-shift-active');
     });
+  });
+
+  /**
+   * Voucher request modal form
+   */
+  const requestModal = new bootstrap.Modal('#request-form-container', {
+    keyboard: true
+  });
+
+  document.querySelectorAll('.camp-shift-btn').forEach((item) => {
+    item.onclick = function () {
+      const form = document.querySelector('.webform-submission-voucher-request-form-form');
+      const header = document.querySelector('#shift-name-header');
+      const campShiftElement = form.querySelector('input[name="camp_shift"]');
+      campShiftElement.value = item.dataset.campShift;
+      header.innerText = item.dataset.campShift;
+
+      requestModal.show();
+      return false;
+    }
   });
 
 })();
